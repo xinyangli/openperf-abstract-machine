14fd0faefd033806f2918b79afe0f2d7d6086520
xinyangli <lixinyang411@gmail.com>
2024-12-16 11:42:14 +0800

    klib: add memchr

--- klib/include/klib.h
+++ klib/include/klib.h
@@ -24,6 +24,7 @@ char *strcpy(char *dst, const char *src);
 char *strncpy(char *dst, const char *src, size_t n);
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
+void *memchr(const void *src, int c, size_t n);
 
 
 //stdlib.h
